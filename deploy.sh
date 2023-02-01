a9af671ab72cbcc6e987faefd7565c62b29828e2
Kiril Mitov <kiril@retreaver.com>
2023-02-01 23:21:17 +0200

    feat: dns - moving to code.cambrian.gg

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,12 @@
 # git status also does not show if the n
 npm link scratch-vm
 
+# npm sometimes does not produce a new file when linking unlinked was done.
+# This brakes us. We need to do it. That's why we generate
+# a new version on every deploy - just the next one and include it in the final
+# file. In this way the final file is new and the hash is new and hew
+# files are delivered from the server
+# npm version prerelease --preid=cambrian # --no-git-tag-version
+
 npm run build
-rsync -avzh  -e 'ssh -p 1022' ./build/* robopart@sega.superhosting.bg:/home/robopart/cambrian.axlessoft.com/ --delete
+rsync -avzh  -e 'ssh -p 1022' ./build/* robopart@sega.superhosting.bg:/home/robopart/code.cambrian.gg/ --delete
